46348b9b77cef39ba1d243d2160151a0897cb836
bscobe4 <78442276+bscobe4@users.noreply.github.com>
2021-04-26 23:30:31 -0700

    Added copyright header

--- ADC Uno Test.ino
+++ ADC Uno Test.ino
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2020
+Steward Observatory Engineering & Technical Services, University of Arizona
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0 which is available at
+http://www.eclipse.org/legal/epl-2.0.
+*/
+
 #include <ADS1256.h>
 #include <SPI.h>
 
