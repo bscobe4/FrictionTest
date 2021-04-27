eaff2eaae7a6121dff25edf9c4a0b816d1afe3cd
bscobe4 <78442276+bscobe4@users.noreply.github.com>
2021-04-26 23:45:33 -0700

    Added copyright header

--- Friction_Test.ino
+++ Friction_Test.ino
@@ -1,4 +1,12 @@
+/*
+Copyright (c) 2020
+Steward Observatory Engineering & Technical Services, University of Arizona
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0 which is available at
+http://www.eclipse.org/legal/epl-2.0.
+*/
 
+/* This friction test makes measurements*/
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
